fda13cc2574e255c17a26b86ac336df5de86b707
deviprasadkl <deviprasadofficial@gmail.com>
2024-09-05 10:00:48 +0530

    ref: background removed

--- src/components/views/home/Home.jsx
+++ src/components/views/home/Home.jsx
@@ -33,7 +33,6 @@ export default function Home() {
               width: 220,
               textAlign: 'center',
               p: 2,
-              background: 'primary',
               border: '2px solid #00A9B9',
               cursor: card.active ? 'pointer' : 'not-allowed',
               transition: 'transform 0.3s ease',
